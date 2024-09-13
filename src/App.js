41921839eab900705d7e9810b86865a4c99614ea
Athar-Naveed <asphaltlegends24@gmail.com>
2024-09-13 18:47:41 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reglo from './components/reglo';
+import ProtectedRoute from './auth/ProtectedRoute';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Reglo />} />
+        <Route path="/" element={<ProtectedRoute element={<Home />} />} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
+    </Router>
+    </>
   );
 }
 
